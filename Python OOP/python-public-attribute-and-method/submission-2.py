0cad0d0095b2688c85aa59ce00b0b3e01d333df8
Illia Khvost <illyahvost@gmail.com>
2026-07-23 10:59:59 +0200

    Add: python-public-attribute-and-method - submission-2

---
+++ Python OOP/python-public-attribute-and-method/submission-2.py
@@ -0,0 +1,8 @@
+class StoreItem:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+chips = StoreItem("Chips", 1.99) # Don't modify this line
+print(chips.name)
+print(chips.price)
